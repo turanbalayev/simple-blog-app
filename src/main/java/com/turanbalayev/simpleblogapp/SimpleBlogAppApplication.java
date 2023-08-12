6503febeaac3c529a31b024971e7e35d65e38d3f
Turan Balayev <turanbalayev96@gmail.com>
2023-08-12 15:55:04 +0400

    Setup project's structure

---
+++ src/main/java/com/turanbalayev/simpleblogapp/SimpleBlogAppApplication.java
@@ -0,0 +1,13 @@
+package com.turanbalayev.simpleblogapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBlogAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBlogAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/turanbalayev/simpleblogapp/SimpleBlogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turanbalayev.simpleblogapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBlogAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
